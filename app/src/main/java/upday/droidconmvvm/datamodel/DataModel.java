a10ea1d2633dfe928183fb237e5c5a1fa75beca5
Florina Muntenescu <florina.muntenescu@gmail.com>
2016-10-19 18:29:09 +0200

    Removed own log.

--- app/src/main/java/upday/droidconmvvm/datamodel/DataModel.java
+++ app/src/main/java/upday/droidconmvvm/datamodel/DataModel.java
@@ -1,7 +1,6 @@
 package upday.droidconmvvm.datamodel;
 
 import android.support.annotation.NonNull;
-import android.util.Log;
 
 import java.util.Arrays;
 import java.util.List;
@@ -9,8 +8,6 @@ import java.util.List;
 import rx.Observable;
 import upday.droidconmvvm.model.Language;
 
-import static android.os.Looper.getMainLooper;
-import static java.lang.Thread.currentThread;
 import static upday.droidconmvvm.model.Language.LanguageCode;
 
 public class DataModel implements IDataModel {
@@ -28,8 +25,6 @@ public class DataModel implements IDataModel {
     @NonNull
     @Override
     public Observable<String> getGreetingByLanguageCode(@NonNull final LanguageCode code) {
-        boolean main = getMainLooper().getThread().equals(currentThread());
-        Log.d("flo", " is main " + main);
         switch (code) {
             case DE:
                 return Observable.just("Guten Tag!");
