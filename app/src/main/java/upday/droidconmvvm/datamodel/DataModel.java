ad93e4e70fdcf0531e02fc049b76262bc9f79270
Venkatesh Burgula <venkateshreddy.burgula@gmail.com>
2017-11-27 15:16:43 -0800

    upgrade to rxjava2 -- address PR comments

--- app/src/main/java/upday/droidconmvvm/datamodel/DataModel.java
+++ app/src/main/java/upday/droidconmvvm/datamodel/DataModel.java
@@ -1,14 +1,11 @@
 package upday.droidconmvvm.datamodel;
 
 import android.support.annotation.NonNull;
-
 import java.util.Arrays;
 import java.util.List;
-
-//import rx.Observable;
 import io.reactivex.Observable;
-import upday.droidconmvvm.model.Language;
 
+import upday.droidconmvvm.model.Language;
 import static upday.droidconmvvm.model.Language.LanguageCode;
 
 public class DataModel implements IDataModel {
